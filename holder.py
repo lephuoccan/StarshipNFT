760e94974bd0fea3db47365b606ff4cbfbe1f910
Le Phuoc Can <32889270+lephuoccan@users.noreply.github.com>
2023-12-24 12:58:28 +0000

    add address count

--- holder.py
+++ holder.py
@@ -15,9 +15,21 @@ def write_to_excel(data, excel_file):
     df = pd.DataFrame(data)
     df.to_excel(excel_file, index=False, engine='openpyxl')
 
+def count_and_write_to_excel(data, excel_file):
+    # Tạo DataFrame từ dữ liệu
+    df = pd.DataFrame(data)
+
+    # Đếm số lần xuất hiện của mỗi địa chỉ
+    address_counts = df['address'].value_counts().reset_index()
+    address_counts.columns = ['address', 'count']
+
+    # Ghi vào file Excel mới
+    address_counts.to_excel(excel_file, index=False, engine='openpyxl')
+
 def main():
     api_url = "https://www.vicscan.xyz/api/nft/inventory"
-    excel_file = "nft_data.xlsx"
+    excel_file_raw_data = "nft_raw_data.xlsx"
+    excel_file_address_counts = "address_counts.xlsx"
     limit_per_page = 100
     total_items = 10000
 
@@ -29,8 +41,11 @@ def main():
         nft_data = get_data_from_api(api_url, offset, limit_per_page)
         all_data.extend(nft_data)
 
-    # Ghi dữ liệu vào file Excel
-    write_to_excel(all_data, excel_file)
+    # Ghi dữ liệu gốc vào file Excel
+    write_to_excel(all_data, excel_file_raw_data)
+
+    # Đếm số lần xuất hiện của địa chỉ và ghi vào file Excel mới
+    count_and_write_to_excel(all_data, excel_file_address_counts)
 
 if __name__ == "__main__":
     main()
